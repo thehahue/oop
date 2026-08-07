d37c1efaeb40680d603df711750367695b2ed6fb
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2026-08-07 12:52:36 +0200

    feat(uebung03): Polymorphie und Bindung veranschaulichen

--- src/main/java/at/bbrz/oop/uebung03_polymorphie/Uebung03.java
+++ src/main/java/at/bbrz/oop/uebung03_polymorphie/Uebung03.java
@@ -8,10 +8,16 @@ public class Uebung03 {
                 new Hausmeister("Herr Kurz")
         };
 
+        // Polymorphie -> Late Binding
+        //                Die Methode wird erst zur Programmlaufzeit bestimmt, die aufgerufen wird (Schuler, Lehrkraft, ...)
         for (Schulmitglied mitglied : mitglieder) {
-            mitglied.beschreiben();
+            System.out.println(mitglied.gibTaetigkeit());
         }
 
+        // Polymorphie -> Early Binding
+        //                Die Methode wird zur Compilezeit bestimme, die aufgerufen wird (Schuler.gibTaetigkeit())
+        Schueler heidi = new Schueler("Heidi");
+        System.out.println(heidi.gibTaetigkeit());
         // TODO: Fuege eine Direktor-Klasse hinzu. Die Schleife darf unveraendert bleiben.
     }
 }
