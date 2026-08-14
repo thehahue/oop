b9eb0617da023f3d187ceed8ee5d42ac264868f0
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2026-08-14 11:44:59 +0200

    Fix caretaker activity punctuation

--- src/main/java/at/bbrz/oop/uebung03_polymorphie/Hausmeister.java
+++ src/main/java/at/bbrz/oop/uebung03_polymorphie/Hausmeister.java
@@ -7,6 +7,6 @@ public class Hausmeister extends Schulmitglied {
 
     @Override
     public String gibTaetigkeit() {
-        return "kuemmert sich um das Schulgebaeude";
+        return "kuemmert sich um das Schulgebaeude.";
     }
 }
