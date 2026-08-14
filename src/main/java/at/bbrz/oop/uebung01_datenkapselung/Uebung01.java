58e4b041aed373fac01a1c2f3d3c1bda66a4eea1
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2026-08-14 09:35:01 +0200

    Refactoring: Add try catch

--- src/main/java/at/bbrz/oop/uebung01_datenkapselung/Uebung01.java
+++ src/main/java/at/bbrz/oop/uebung01_datenkapselung/Uebung01.java
@@ -2,14 +2,18 @@ package at.bbrz.oop.uebung01_datenkapselung;
 
 public class Uebung01 {
     public static void main(String[] args) {
-        Schueler mia = new Schueler("Mia", 15);
-        mia.vorstellen();
-        //mia.setAlter(16);
-        System.out.println("Alter: " + mia.getAlter());
 
-        Schueler franz = new Schueler("Franz", 18);
-        franz.vorstellen();
+        try {
+            Schueler mia = new Schueler("Mia", 15);
+            mia.vorstellen();
+            //mia.setAlter(16);
+            System.out.println("Alter: " + mia.getAlter());
 
+            Schueler franz = new Schueler("Franz", -1);
+            franz.vorstellen();
+        } catch (Exception e) {
+            System.out.println("Fehler: " + e.getMessage());
+        }
         // Probiere aus: Warum ist mia.alter = -3; nicht erlaubt?
     }
 }
